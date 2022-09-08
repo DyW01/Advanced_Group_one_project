d77f2528b32d03ed7d2266e290b280c27e1cb65a
Akash Kapali <akash.kapali@uib.no>
2022-09-08 10:53:42 +0200

    Akash's changes

---
+++ Collab_script.R
@@ -0,0 +1,17 @@
+#The epic code of collaboration
+#Git power!
+
+#we shall multiply
+
+#first attempt
+
+#Assigning variables 
+a <- 5
+b <- 8
+
+# add formula
+
+# Akash's addition
+x<-7
+y<-9
+multiplication<- x*y
