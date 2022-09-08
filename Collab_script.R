31e2535cf8b2e156d48388607a713f0321e71616
Shwe Sin <shwe.win@uib.no>
2022-09-08 10:50:11 +0200

    new line

---
+++ Collab_script.R
@@ -0,0 +1,14 @@
+#The epic code of collaboration
+#Git power!
+
+#we shall multiply
+
+#first attempt
+
+#Assigning variables 
+a <- 5
+b <- 8
+
+# add formula
+
+#check my branch if it works or not (SS)
