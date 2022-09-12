f13fb3ab555009d09216ded080ffb5110b973133
DyW01 <danial.w91@gmail.com>
2022-09-12 14:51:57 +0200

    added script structure

--- Data/DYW_Exam_Script.R
+++ Data/DYW_Exam_Script.R
@@ -6,7 +6,14 @@ library ("here")
 #Reading the text file
 read_delim("exam_nontidy.txt", delim = "\t")
 
-#FOllowinge error: 'exam_nontidy.txt' does not exist in current working directory ('C:/Users/Icebe/OneDrive/Skrivebord/R statistics course/Tasks and other files/Advanced_Group_one_project').
-#Change directory in settings within the files pane
+#Followinge error: 'exam_nontidy.txt' does not exist...
+#Change directory in settings in the files pane
 
-#View                                                                  
+#Assign datafile to name, my_data
+my_data <- read_delim("exam_nontidy.txt", delim = "\t")
+my_data
+
+#View mydata | NB! if data table is too huge, not recommended
+view(my_data)
+
+#
