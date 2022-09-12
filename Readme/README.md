823f1d76145f778aaf90c350e5c7476735ebc31d
DyW01 <danial.w91@gmail.com>
2022-09-12 15:07:09 +0200

    rydding process

--- Readme/README.md
+++ Readme/README.md
@@ -8,3 +8,19 @@ Hello guys!
 
 
 #We shall pass!
+
+
+#Following description of data:
+#------ Description of dataset ----------#
+#Notes from codebook:
+
+#We have two cases of meningites:
+  #viral (abm = 0) vs. bacterial (abm = 1) cases. 
+  #Total of 581 patients, and missing data accordingly from n amount of patients
+  #There are samples from spinal fluid, and blood
+  #Cases from year 1968 to 1980
+  #Population race and sex are defined
+
+#My Day_One task is to identify duplicates, and merge files from everyone
+
+
--- Scripts/DYW_Exam_Script.R
+++ Scripts/DYW_Exam_Script.R
@@ -1,26 +1,19 @@
-#loadin library
-
+#My branch script to tidy data
+#load packages
 library(tidyverse)
 library ("here")
-#checking the directory
-here()
 
-#Reading the text file, working first on nontidy
+#Reading the text file
 read_delim("exam_nontidy.txt", delim = "\t")
-#if it doesnt work, change directory by "setting" in file window
 
-#View it to understand what to work with
-view(read_delim("exam_nontidy.txt", delim = "\t"))
+#Followinge error: 'exam_nontidy.txt' does not exist...
+#Change directory in settings in the files pane
 
-#Now to seperate the tasks with the tidy process
-#SS seperate feature into sex and race
-#Akash rewriting age colum
-#Theo renaming date into date of birth
-#DYW Removing duplicated row
-
+#Assign datafile to name, my_data
 my_data <- read_delim("exam_nontidy.txt", delim = "\t")
+my_data
 
+#View mydata | NB! if data table is too huge, not recommended
 view(my_data)
 
-skimr::skim(my_data)
-
+#
