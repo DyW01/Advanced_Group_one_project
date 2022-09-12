841d60d3b7fed4b86f357517d54c50dfd51a9f7b
DyW01 <danial.w91@gmail.com>
2022-09-12 12:29:13 +0200

    Added my script

---
+++ Readme/DYW_Exam_Script.R
@@ -0,0 +1,21 @@
+#loadin library
+
+library(tidyverse)
+library ("here")
+#checking the directory
+here()
+
+#Reading the text file, working first on nontidy
+read_delim("exam_nontidy.txt", delim = "\t")
+#if it doesnt work, change directory by "setting" in file window
+
+#View it to understand what to work with
+view(read_delim("exam_nontidy.txt", delim = "\t"))
+
+#Now to seperate the tasks with the tidy process
+#SS seperate feature into sex and race
+#Akash rewriting age colum
+#Theo renaming date into date of birth
+#DYW Removing duplicated row
+
+
