aed8fbaac832cc1ad5ee3213e79c5530dd57d222
Aditi Singh <weird>
2022-09-13 09:40:02 +0200

    Aditi - Day 5 file. Introduction to coding in R and file manipulation/wrangling.

---
+++ Scripts/Aditi_Exam_Script.R
@@ -0,0 +1,103 @@
+#--------------------------------------------#
+#Day 5
+#--------------------------------------------#
+
+#Install and load packages
+# install.packages("tidyverse")
+library(tidyverse)
+library(here)
+#Change working directory
+
+
+
+#Load file into work environment
+#Separate columns using delimiter ("tab" - \t)
+
+read_delim("exam_nontidy.txt", delim = "\t")
+data_untidy <- read_delim("exam_nontidy.txt", delim = "\t")
+
+#view(data_untidy) - not recommended for large datasets
+head(data_untidy) # columns store values as <chr> which need to be converted to <numeric>
+summary(data_untidy)
+glimpse(data_untidy)
+data_untidy %>% 
+  distinct()
+data_untidy %>% 
+  count(date)
+
+#Duplicates
+data_untidy %>% 
+  count(id, sort = TRUE)
+
+#Maximum and Minimum
+data_untidy %>% 
+  summarize(max(blood_wbc, na.rm = T), 
+            min(blood_wbc, na.rm = T), 
+            mean(blood_wbc,na.rm = T))
+
+
+#Select
+data_untidy %>% 
+  select(age_1, sex) %>% 
+  head()
+
+#Change order of columns
+data_untidy <-
+  data_untidy %>% 
+  select(id, sex, race, starts_with("csf"), starts_with("blood"))
+
+
+#Provides summary statistics - missing, complete, n and sd
+#https://www.rdocumentation.org/packages/skimr/versions/2.1.4
+skimr::skim(data_untidy) 
+
+#Graph showing frequency of missing for each variable
+naniar::gg_miss_var((data_untidy))
+
+
+
+#-------------------------------------#
+#The table is currently in the cross-sectional format. 
+#Multiple occurence of same ID.
+#-------------------------------------#
+
+data_untidy$`feature type` %>% 
+  head()
+
+
+#Rename column name to "feature_type"
+#data_untidy <- rename(data_untidy, feature_type = `feature type`)
+data_untidy <-
+  data_untidy %>% 
+  rename(feature_type = `feature type`,
+         age_1 = `1.age`)
+
+#Pivot "feature type" into "sex" and "race"
+data_untidy <- pivot_wider(data_untidy, names_from = feature_type, values_from = feature_value )
+
+#Date - two columns - year and month
+
+
+#Turning a string column into numeric 
+data_untidy <-
+  data_untidy %>% 
+  mutate(csf_prot = as.numeric(csf_prot))
+
+
+#Creating a new ID column
+data_untidy <-
+  data_untidy %>% 
+  mutate(ID_new = 1:n())
+
+#Comparison of two columns
+data_untidy %>% 
+  select(id, ID_new) %>% 
+  mutate(if_else(
+    as.character(id) == as.character(ID_new), "equal", "different"
+  ))
+
+#Removes column "ID_new"
+new_data <- subset(data_untidy, 
+                   select = -c(ID_new))
+
+
