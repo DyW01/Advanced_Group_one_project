7cace5a3ce5f99318cd7866aea0e45c284e6dde7
hatheos <habumutheos2020@gmail.com>
2022-09-13 10:36:29 +0200

    Second changes onn 13 Sept 2022

--- theogene_exam_script.R
+++ theogene_exam_script.R
@@ -4,5 +4,23 @@
 #Loading library
 
 library(tidyverse)
+library(here)
+
+#Reading the data file
+
+read_delim("exam_nontidy.txt", delim =" \t")
+
+#Now the dataset is loaded in my working directory 
+
+
+#Viewing the dataset:
+view(read_delim("exam_nontidy.txt", delim =" \t"))
+
+#Theo will re-name variable date into date of birth
+
+
+
+
+
 
 
